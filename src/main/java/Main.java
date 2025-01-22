460f86b5c6b9dce88654c473ca61eb438108d583
Crisveling <crisvelingg@gmail.com>
2025-01-22 19:51:33 +0100

    Cambio de saludo en feature/cambio-saludo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 public static void main(String[] args) {
-System.out.println("Hola, control de versiones con Git");
+System.out.println("¡Hola desde la rama feature/cambio-saludo!");
 System.out.println("Nueva funcionalidad: Mensaje de bienvenida.");
 }
 }
